0346f30baf9d0c611b50b55feef15d6f82eed798
Eros Karsburg <eroskarsburg@gmail.com>
2024-07-04 15:59:40 -0300

    fix: adjusted tables

--- MindCare-Central-Clinic/wwwroot/js/site.js
+++ MindCare-Central-Clinic/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿function OpenPaymentInfo(seta) {
+﻿
+//window.onload = function () {
+//    Util.LimitTableRows(this);
+//}
+
+
+function OpenPaymentInfo(seta) {
 
     if ($("#" + seta).hasClass("active")) {
         $("#" + seta).removeClass("rotate");
@@ -45,5 +51,54 @@ var Util = {
                 }
             }
         }
-    }
+    },
+    Sort: function (n, table) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById(table);
+        switching = true;
+        dir = "asc";
+        while (switching) {
+            switching = false;
+            rows = table.rows;
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount++;
+            } else {
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    },
+    //LimitTableRows: function (tableName) {
+    //    const rowCount = document.getElementById('rowCount').value;
+    //    const table = document.getElementById(tableName);
+    //    const rows = table.getElementsByTagName('tr');
+
+    //    for (let i = 1; i < rows.length; i++) {
+    //        if (i <= rowCount) {
+    //            rows[i].style.display = '';
+    //        } else {
+    //            rows[i].style.display = 'none';
+    //        }
+    //    }
+    //},
 }
